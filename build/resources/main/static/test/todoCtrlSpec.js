19bca008268fa53c7eeb45ce121dd09b4e443fc5
jocelio <jocelio27@gmail.com>
2018-05-21 13:58:12 -0300

    angular folders realocation

---
+++ build/resources/main/static/test/todoCtrlSpec.js
@@ -0,0 +1,35 @@
+/*global describe, it, beforeEach, inject, expect*/
+(function () {
+	'use strict';
+
+	describe('Todo Controller', function () {
+		var ctrl, scope, store;
+
+		// Load the module containing the app, only 'ng' is loaded by default.
+		beforeEach(module('todomvc'));
+
+		beforeEach(inject(function ($controller, $rootScope, localStorage) {
+			scope = $rootScope.$new();
+
+			store = localStorage;
+
+			localStorage.todos = [];
+			localStorage._getFromLocalStorage = function () {
+				return [];
+			};
+			localStorage._saveToLocalStorage = function (todos) {
+				localStorage.todos = todos;
+			};
+
+			ctrl = $controller('TodoCtrl', {
+				$scope: scope,
+				store: store
+			});
+		}));
+
+		it('should not have an edited Todo on start', function () {
+			expect(scope.editedTodo).toBeNull();
+		});
+
+	});
+}());
---
+++ src/main/resources/static/test/directivesSpec.js
@@ -0,0 +1,12 @@
+/*global describe, it, beforeEach, inject, expect, angular*/
+(function () {
+	'use strict';
+
+	beforeEach(module('todomvc'));
+
+	describe('todoFocus directive', function () {
+		var scope, compile, browser;
+
+
+	});
+}());
