99fd9ee93f42311b8c1bcd3beaa2c6d9581b97b7
jocelio <jocelio27@gmail.com>
2018-05-23 23:48:35 -0300

    securing atm resource

--- src/main/java/br/gov/smartatm/config/OAuth2ServerConfiguration.java
+++ src/main/java/br/gov/smartatm/config/OAuth2ServerConfiguration.java
@@ -55,7 +55,8 @@ public class OAuth2ServerConfiguration {
 			http
 				.authorizeRequests()
 					.antMatchers("/users").hasRole("ADMIN")
-					.antMatchers("/greeting").authenticated();
+					.antMatchers("/greeting").authenticated()
+					.antMatchers("/api/**").authenticated();
 		}
 
 	}
